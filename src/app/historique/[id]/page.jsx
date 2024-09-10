00db828558ef4b15c72b4635a6a1dea22b869a55
faridmth <methia.farid2001@gmail.com>
2024-09-11 00:05:42 +0200

    changed the db provider from posgresql to mysql

--- src/app/historique/[id]/page.jsx
+++ src/app/historique/[id]/page.jsx
@@ -25,15 +25,18 @@ const Page = ({ params }) => {
   return (
     <div className='ml-[250px]'>
       {isLoading ? (
-        <div className="w-full flex justify-center mt-16">
+        <div className="w-full flex justify-center pt-16">
           <h1 className="text-xl">Loading...</h1>
         </div>
       ) : (
         <div>
+          <div className="w-full pl-16 pt-14">
+            <h2 className="text-xl "><span className="font-medium">Id de l&apos;entité :</span> {decodeURIComponent(params.id)}</h2>
+          </div>
           {historique && historique.length > 0 ? (
             <Table data={historique} />
           ) : (
-            <div className="w-full flex justify-center mt-16">
+            <div className="w-full flex justify-center pt-16">
               <h1 className="text-xl">une erreur c&apos;est produit veuillez réessayer</h1>
             </div>
           )}
